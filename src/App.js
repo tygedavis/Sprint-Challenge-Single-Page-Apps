8dc27a483f0293bf6eef9255b7494d9dedcb89b7
tygedavis <tyge.davis@gmail.com>
2019-11-15 09:18:51 -0700

    Added links to the home page and the character page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route, Link } from 'react-router-dom'
+import WelcomePage from './components/WelcomePage'
+import CharacterList from './components/CharacterList'
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Link to='/home'>Home</Link>
+      <Link to='/characters'>Character List</Link>
+
+
+      <Route path='/home' component={ WelcomePage }/>
+      <Route path='/characters' component={ CharacterList }/>
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState();
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+    axios.get()//This grabs the data
   }, []);
 
   return (
