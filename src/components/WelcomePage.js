5c72a49f6e1032684fd76f0b0a2d05cf11744bc0
tygedavis <tyge.davis@gmail.com>
2019-11-15 09:51:20 -0700

    Styled Home page using styled-components

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,11 +1,18 @@
 import React from "react";
+import styled from 'styled-components'
+
+const StyledImg = styled.img`
+  display: flex;
+  justify-content: center;
+  margin: 10px auto;
+`;
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <StyledImg
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
